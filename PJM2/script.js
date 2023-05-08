3d7ff5635b52cbf591fc13a48bf114c7b2c01586
0Jaemin0 <jaemin2167@naver.com>
2023-05-08 21:29:31 +0900

    두번째 과제 제출

---
+++ PJM2/script.js
@@ -0,0 +1,62 @@
+//문제 1
+// var nums = [100, 200, 300, 400, 500];
+// nums.splice(3,2);
+// console.log(nums);
+
+//문제 2
+// var arr = [200, 100, 300];
+// arr.splice(2, 0, 10000);
+// console.log(arr);
+
+//문제 3
+// 4 - object
+
+//문제 4
+// 2 - 입력 : a = 2.22, 출력 : boolean
+
+//문제5
+// 4 - 16
+
+//문제6
+// 2 - 1
+
+//문제7
+// 3 - let
+// 5 - 1age
+
+//문제8
+// 84
+
+//문제9
+// var year = '2019';
+// var month = '04';
+// var day = '26';
+// var hour = '11';
+// var minute = '34';
+// var second = '27';
+
+// var result = `${year}/${month}/${day} ${hour}:${minute}:${second} `
+// console.log(result);
+
+//문제10
+// let num = prompt("정수 입력");
+// document.write("입력", '<br>');
+// document.write(num, '<br>', '<br>');
+
+// document.write("출력", '<br>');
+// for(let i = 1; i <= num; i++) { //12345
+//     for(let j = num-i; j >= 0; j--) {
+//         document.write("\u00a0"); //43210
+//     }
+
+//     for(let k = 0; k < i*2-1; k++) {
+//         document.write("*"); //13579
+//     }
+//     document.write('<br>');
+// }
+
+
+
+
+
+
