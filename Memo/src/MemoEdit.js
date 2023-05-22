6a9a3d2fb48eb8d411203624d54488440e6e2ff8
Je-DG <gyuni6652@naver.com>
2023-05-23 03:41:43 +0900

    세번째 과제 완료

--- Memo/src/MemoEdit.js
+++ Memo/src/MemoEdit.js
@@ -1,28 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const MemoEdit = () => {
+const MemoEdit = ({onCreate}) => {
+  const [state, setState] = useState({
+    title: "",
+    content: "",
+    member: "",
+  });
+  const handleState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]:e.target.value,
+    })
+  }
+  const handleSub = (e) => {
+    console.log(state)
+    onCreate(state.title, state.content, state.member);
+    alert('저장성공');
+    setState({    
+      title: "",
+    content: "",
+    member: ""})
+  }
 
   return (
     <div className="MemoEdit">
     <h2>오늘의 강의 메모</h2>
     <div>
-      <input placeholder='제목'/>
+      <input
+      className='Input' 
+      placeholder='제목'
+      name = 'title'
+      value={state.title}
+      onChange={handleState}
+      />
     </div>
     <div>
-      <textarea placeholder='내용작성'/>
+      <textarea
+      className='Textarea' 
+      placeholder='내용작성'
+      name ='content'
+      value={state.content}
+      onChange={handleState}
+      />
     </div>
     <div>
       <span>운영진 : </span>
-      <select>
-        <option value={1}>안재현</option>
-        <option value={2}>이태헌</option>
-        <option value={3}>오세찬</option>
-        <option value={4}>모유진</option>
-        <option value={5}>박규현</option>
+      <select name='member' value={state.member} onChange={handleState}>
+        <option value='default'>운영진을 선택해주세요</option>
+        <option value={'안재현'}>안재현</option>
+        <option value={'이태헌'}>이태헌</option>
+        <option value={'오세찬'}>오세찬</option>
+        <option value={'모유진'}>모유진</option>
+        <option value={'박규현'}>박규현</option>
       </select>
     </div>
     <div>
-      <button>저장하기</button>
+      <button onClick={handleSub}>저장하기</button>
     </div>
   </div>
   );
