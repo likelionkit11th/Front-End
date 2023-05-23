3307f855ad5b0fb6ffe136f23dabf7ae47853a42
bini59 <myuni0234@gmail.com>
2023-05-23 17:13:33 +0900

    style : 다크모드 추가

--- LYB3/Memo/src/App.js
+++ LYB3/Memo/src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './App.css';
 import MemoEdit from './MemoEdit';
 import MemoList from './MemoList';
 
 function App() {
-
+  const [isDark, setIsDark] = useState(false);
   const [data, setData] = useState([]);
 
   const onCreate = (title, content, member) => {
@@ -13,8 +13,19 @@ function App() {
     setData([ newItem, ...data ]);
   }
 
+  const darkMode = () => {
+    setIsDark(!isDark);
+  }
+
+  useEffect(() => {
+    let classname = "App " + isDark ? "darkmode" : "";
+    let text = isDark ? "다크모드 활성화" : "다크모드 비활성화";
+    console.log(text);
+  }, [isDark]);
+
   return (
-    <div className="App">
+    <div className={"App " + (isDark ? "darkmode" : "")}>
+      <button onClick={darkMode}>{isDark ? '🌝': '🌚'}</button>
       <MemoEdit onCreate={onCreate} />
       <MemoList list={data} />
     </div>
