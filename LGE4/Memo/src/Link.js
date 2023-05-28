b1e4270cb45cdfa77d9cb18667d863c5500cd1ae
Commeci <cjtkemfldj@naver.com>
2023-05-28 20:20:01 +0900

    fourth

---
+++ LGE4/Memo/src/Link.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+<Link to="경로"></Link>
+
+const navigate = useNavigate();
+
+navigate("링크")
+navigate("/create")
