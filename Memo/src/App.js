cb811f96c6ad7d30c23821d7c12ee4a366c33419
bini59 <myuni0234@gmail.com>
2023-06-05 22:35:51 +0900

    fix : 기능 수정
    - 이미지 여러개 출력되는 오류 수정
    - 이미지 클릭시 색상 제대로 적용되지 않는 오류 수정
    
    - localstorage이용하여 저장 및 불러오기 기능 생성

--- Memo/src/App.js
+++ Memo/src/App.js
@@ -10,20 +10,18 @@ function App() {
 
   // 임시 데이터 삽입
   useEffect(() => {
-    let temp = [
-      { title: "임시 1", content: "임시 내용 1", emotion: "1" },
-      { title: "임시 2", content: "임시 내용 2", emotion: "2" },
-      { title: "임시 3", content: "임시 내용 3", emotion: "3" },
-      { title: "임시 4", content: "임시 내용 4", emotion: "4" },
-      { title: "임시 5", content: "임시 내용 5", emotion: "5" },
-      { title: "임시 6", content: "임시 내용 6", emotion: "3" },
-    ];
-    setMemo(temp);
+    let temp = JSON.parse(localStorage.getItem("diary"))
+    setMemo(!temp ? [] : temp);
   }, []);
+
+  const onCreate = (item) => {
+    setMemo([...memo, item])
+    localStorage.setItem("diary", JSON.stringify([...memo, item]))
+  }
 
   return (
     <div className="App">
-      <MemoEdit />
+      <MemoEdit onCreate={onCreate}/>
       <Viewer memos={memo} />
     </div>
   );
